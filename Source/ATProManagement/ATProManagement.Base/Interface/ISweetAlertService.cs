c22af380b432ac7a2ce2e082925aa0054eb1163f
anhVuNguyen-X3L <150757234+anhVuNguyen-X3L@users.noreply.github.com>
2026-05-20 16:16:48 +0700

    Create ISweetAlertService.cs

---
+++ Source/ATProManagement/ATProManagement.Base/Interface/ISweetAlertService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATProManagement.Base
+{
+    public interface ISweetAlertService
+    {
+        Task Success(string message, string title = "Success");
+
+        Task Error(string message, string title = "Error");
+
+        Task Warning(string message, string title = "Warning");
+
+        Task<bool> Confirm(string message, string title = "Confirm");
+
+        Task Loading(string title = "Loading...");
+
+        Task Close();
+
+        Task Toast(string message);
+    }
+}
