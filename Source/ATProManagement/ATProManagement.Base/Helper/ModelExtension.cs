99441ac6a5cc9142d1dd43b0a85d2b44197e9e7d
anhVuNguyen-X3L <150757234+anhVuNguyen-X3L@users.noreply.github.com>
2026-05-21 15:14:40 +0700

    covert entity to model

---
+++ Source/ATProManagement/ATProManagement.Base/Helper/ModelExtension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ATProManagement.Base;
+
+public static class ModelExtension
+{
+    public static TDestination ToModel<TDestination>(this object source)
+       where TDestination : new()
+    {
+        if (source == null)
+            return default!;
+
+        var destination = new TDestination();
+
+        var sourceProperties = source.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var destinationProperties = typeof(TDestination).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var destProp in destinationProperties)
+        {
+            if (!destProp.CanWrite)
+                continue;
+
+            var sourceProp = sourceProperties.FirstOrDefault(x =>
+                x.Name == destProp.Name &&
+                x.PropertyType == destProp.PropertyType);
+
+            if (sourceProp == null)
+                continue;
+
+            var value = sourceProp.GetValue(source);
+
+            destProp.SetValue(destination, value);
+        }
+
+        return destination;
+    }
+}
+
